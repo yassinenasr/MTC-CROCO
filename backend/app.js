27b0d307a8f1ef268cca4002c24e51dfaffbfcf5
yassinenasr <yassine.nasrenetcom@gmail.com>
2025-02-12 16:39:05 +0100

    séance2

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+// import express module 
+const express = require("express");
+
+// creates express app 
+const app = express();
+
+// make app exportable
+// app est utilisable dans d'autres fichiers 
+module.exports = app;
